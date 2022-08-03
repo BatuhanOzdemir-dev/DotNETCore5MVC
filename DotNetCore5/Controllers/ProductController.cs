5a6ea4cf0d8900d6ad3b21b399dd7e6e15e360c8
Batuhan Özdemir <batuhan22.ozdemir@gmail.com>
2022-08-03 10:58:49 +0300

    Add project files.

---
+++ DotNetCore5/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCore5.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult GetProducts()
+        {
+            return View();
+        }
+    }
+}
