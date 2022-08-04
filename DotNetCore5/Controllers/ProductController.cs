87665063d02e1db3ab632e47bd8acebf86b2e525
Batuhan Özdemir <batuhan22.ozdemir@gmail.com>
2022-08-04 19:59:45 +0300

    Examined multiple action return types and the reason to be using IActionResult.

--- DotNetCore5/Controllers/ProductController.cs
+++ DotNetCore5/Controllers/ProductController.cs
@@ -1,11 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DotNetCore5.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DotNetCore5.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult GetProducts()
+        //public IActionResult GetProducts()
+        //{
+        //    return View();
+        //}
+        public ViewResult ViewProducts()
         {
+            return View();
+        }
+        public PartialViewResult PartialViewProducts()
+        {
+            //Client tabanlı Ajax isteklerinde kullanıma yatkındır.
+            return PartialView();
+        }
+        public JsonResult JsonProducts()
+        {
+            JsonResult result = Json(new Product
+            {
+                Id = 5,
+                ProductName = "Terlik",
+                Quantity = 15
+            });
+            return result;
+        }
+        public EmptyResult Empty()
+        {
+            //Can also use the void return type for the same effect.
+            return new EmptyResult();
+        }
+        public ContentResult ContentResult()
+        {
+            //JSON gibi bi da daha çok Ajax için kullanılır.
+            ContentResult result = Content("Content Result Text!");
+            return result;
+        }
+        public ActionResult GetProducts()
+        {
+            //Includes all the return types.
+            return View();
+        }
+        public IActionResult IGetProducts()
+        {
+            //Can return multiple returns.
             return View();
         }
     }
